7ae2bd5f360da75c891842449e3d713f359d529a
Dan <dhgekblom@gmail.com>
2018-05-11 12:17:03 +0200

    Added @ComponentScan annotation

--- src/main/java/com/danekblom/lexicon/finalprojectrest/application/FinalProjectRestApplication.java
+++ src/main/java/com/danekblom/lexicon/finalprojectrest/application/FinalProjectRestApplication.java
@@ -2,8 +2,10 @@ package com.danekblom.lexicon.finalprojectrest.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.danekblom.lexicon.finalprojectrest"})
 public class FinalProjectRestApplication {
 
 	public static void main(String[] args) {
