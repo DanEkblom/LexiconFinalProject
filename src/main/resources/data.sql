ec305ea1e68e7890c6d40a348d588f00db70cf60
DanEkblom <dhgekblom@gmail.com>
2018-05-13 19:46:50 +0200

    First file commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+DELETE FROM tblAudio;
+
+INSERT INTO tblAudio (id, artist, title, year, tracks, length, genre, recordLabel, language, mediaType, audioFormat, barCode)
+VALUES (1, "Pink Floyd", "The Endless River", 2014, 18, "Rock", "Parlophone Records", "English", "CD", "Stereo", "8-25646-21542-3"),
+       (2, "Queen", "Innuendo", 1991, 12, "Rock", "Parlophone Records", "English", "CD", "Stereo", "0-077779-588725")
+       (3, "Ghost", "Meliora", 2015, 10, "Rock", "Universal Music AB", "English", "CD", "Stereo", "6-02547-33055-0"),
+       (4, "Prince", "Art Official Age", 2014, 13, "Funk", "Warner Bros. Records Inc.", "English", "CD", "Stereo", "0-9362-49333-4"),
+       (5, "Rammstein", "Rosenrot", 2005, 11, "Industrial", "Universal Music GMBH", "German", "CD", "Stereo", "0-602498-745885-3");
