7bebbdebc4e4726fee41ec25f0ec1fdbc0b7b2ae
DanEkblom <dhgekblom@gmail.com>
2018-05-14 16:28:49 +0200

    ResponseStatus annotations added
    Took 51 seconds

--- src/main/java/com/danekblom/lexicon/finalprojectrest/controllers/RestAudio.java
+++ src/main/java/com/danekblom/lexicon/finalprojectrest/controllers/RestAudio.java
@@ -3,10 +3,11 @@ package com.danekblom.lexicon.finalprojectrest.controllers;
 import com.danekblom.lexicon.finalprojectrest.model.Audio;
 import com.danekblom.lexicon.finalprojectrest.services.AudioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -24,8 +25,8 @@ public class RestAudio {
      * HTTP GET Method. Returns a list of all Audio items in data source
      * @return List of Audio items
      */
-    @ResponseBody
-    @RequestMapping(value = "/allAudio", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/audio/all", method = RequestMethod.GET)
     public List<Audio> getAll() {
         if (audioService.listAllAudioItems().isEmpty()) {
             System.out.println("Please add a couple of albums."); // For console logging during development
@@ -41,12 +42,9 @@ public class RestAudio {
      * @param audioItem Audio class item to be added
      * @return Audio class
      */
-    @ResponseBody
-    @RequestMapping(value = "/addAudio", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(value = "/audio/add", method = RequestMethod.POST)
     public Audio addAudio(@Valid @RequestBody Audio audioItem) {
-
-        //Audio newAudioItem = new Audio(1, "The Division Bell", 1999, "Pink Floyd", "EMI", MusicGenre.ROCK, 90, 12, MediaType.CD, Language.ENGLISH, AudioFormat.STEREO, "1-234-5678-9");
-        //Audio newAudioItem = new Audio(id, title, year, artist, recordLabel, genre, length, numberOfTracks, mediaType, language, audioFormat, barCode);
 
         return audioService.addAudioItem(new Audio(audioItem.getId(),
                 audioItem.getTitle(),
@@ -71,16 +69,13 @@ public class RestAudio {
                           @PathVariable("var3") Integer year,
                           @PathVariable("var4") String artist,
                           @PathVariable("var5") String recordLabel,
-                          @PathVariable("var6") MusicGenre genre,
+                          @PathVariable("var6") String genre,
                           @PathVariable("var7") Integer length,
                           @PathVariable("var8") Integer numberOfTracks,
-                          @PathVariable("var9") MediaType mediaType,
-                          @PathVariable("var10") Language language,
-                          @PathVariable("var11") AudioFormat audioFormat,
+                          @PathVariable("var9") String mediaType,
+                          @PathVariable("var10") String language,
+                          @PathVariable("var11") String audioFormat,
                           @PathVariable("var12") String barCode) {
-
-        //Audio newAudioItem = new Audio(1, "The Division Bell", 1999, "Pink Floyd", "EMI", MusicGenre.ROCK, 90, 12, MediaType.CD, Language.ENGLISH, AudioFormat.STEREO, "1-234-5678-9");
-        //Audio newAudioItem = new Audio(id, title, year, artist, recordLabel, genre, length, numberOfTracks, mediaType, language, audioFormat, barCode);
 
         return audioService.addAudioItem(new Audio(id, title, year, artist, recordLabel, genre, length, numberOfTracks, mediaType, language, audioFormat, barCode));
 
