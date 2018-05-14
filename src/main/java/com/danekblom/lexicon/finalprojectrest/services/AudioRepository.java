58c0071b94166ef6a54c83c6567643ef05071616
DanEkblom <dhgekblom@gmail.com>
2018-05-14 16:25:03 +0200

    Annotations added
    Took 35 seconds

--- src/main/java/com/danekblom/lexicon/finalprojectrest/services/AudioRepository.java
+++ src/main/java/com/danekblom/lexicon/finalprojectrest/services/AudioRepository.java
@@ -2,6 +2,10 @@ package com.danekblom.lexicon.finalprojectrest.services;
 
 import com.danekblom.lexicon.finalprojectrest.model.Audio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional
 public interface AudioRepository extends JpaRepository<Audio, Integer> {
 }
