50ad504fe8e59a4f6315522f1c64a0f8f07c0fea
DanEkblom <dhgekblom@gmail.com>
2018-05-14 19:00:39 +0200

    @EnableJpaRepositories and @EntityScan annotations added.
    Took 2 minutes

--- src/main/java/com/danekblom/lexicon/finalprojectrest/application/FinalProjectRestApplication.java
+++ src/main/java/com/danekblom/lexicon/finalprojectrest/application/FinalProjectRestApplication.java
@@ -2,10 +2,14 @@ package com.danekblom.lexicon.finalprojectrest.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.danekblom.lexicon.finalprojectrest"})
+@EnableJpaRepositories(basePackages = "com.danekblom.lexicon.finalprojectrest.data")
+@EntityScan(basePackages = "com.danekblom.lexicon.finalprojectrest.model")
 public class FinalProjectRestApplication {
 
 	public static void main(String[] args) {
