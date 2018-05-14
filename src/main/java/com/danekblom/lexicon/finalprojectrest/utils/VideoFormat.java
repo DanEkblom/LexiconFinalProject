cb974bd0bc3fc2c23ddceec27a546b67e6861f82
DanEkblom <dhgekblom@gmail.com>
2018-05-14 16:15:57 +0200

    "DVD" option added
    Took 2 minutes

--- src/main/java/com/danekblom/lexicon/finalprojectrest/utils/VideoFormat.java
+++ src/main/java/com/danekblom/lexicon/finalprojectrest/utils/VideoFormat.java
@@ -2,6 +2,7 @@ package com.danekblom.lexicon.finalprojectrest.utils;
 
 public enum VideoFormat {
     SD,      // < 720p
+    DVD,     // < 720p, DVD
     HDREADY, // 720p
     FULLHD,  // 1080p
     UHD      // 4K
