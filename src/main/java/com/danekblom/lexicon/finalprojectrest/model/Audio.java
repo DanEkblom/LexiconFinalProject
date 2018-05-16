9fa60ed84c4c0c07eeb762346f4cf6961f10b8f5
Dan <dhgekblom@gmail.com>
2018-05-16 16:27:21 +0200

    GenerationType changed from AUTO to IDENTITY

--- src/main/java/com/danekblom/lexicon/finalprojectrest/model/Audio.java
+++ src/main/java/com/danekblom/lexicon/finalprojectrest/model/Audio.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 public class Audio {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false, nullable = false)
     private Integer id;
 
