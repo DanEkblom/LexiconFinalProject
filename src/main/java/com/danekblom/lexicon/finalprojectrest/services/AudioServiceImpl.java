64503f5db6e5f637ff303eabe3b8476acc4b5613
Dan <dhgekblom@gmail.com>
2018-05-17 15:31:14 +0200

    Simplified deleteById and findById methods, TODO added

--- src/main/java/com/danekblom/lexicon/finalprojectrest/services/AudioServiceImpl.java
+++ src/main/java/com/danekblom/lexicon/finalprojectrest/services/AudioServiceImpl.java
@@ -2,14 +2,11 @@ package com.danekblom.lexicon.finalprojectrest.services;
 
 import com.danekblom.lexicon.finalprojectrest.data.AudioRepository;
 import com.danekblom.lexicon.finalprojectrest.model.Audio;
-import com.danekblom.lexicon.finalprojectrest.model.DocumentationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class AudioServiceImpl {
@@ -17,48 +14,66 @@ public class AudioServiceImpl {
     @Autowired
     private AudioRepository audioRepository;
 
-    private Map<String, String> error = new HashMap<>();
-
     public AudioServiceImpl() {
     }
 
+
+    // _C_RUD
     public Audio addAudioItem(Audio newAudio) {
         return audioRepository.save(newAudio);
     }
 
-    public Audio updateAudioItem(Audio audioItemToUpdate) {
-        return audioRepository.save(audioItemToUpdate);
-    }
-
-    public void deleteAudioItem(Audio audioItemToDelete) {
-        if (audioRepository.findAll().contains(audioItemToDelete)) {
-            audioRepository.delete(audioItemToDelete);
-        }
-        else {
-            new DocumentationResponse("Item Not Found.");
-        }
-    }
-
+    // C_R_UD
     public List<Audio> listAllAudioItems() {
 
         return audioRepository.findAll();
     }
 
     public Audio findById(Integer id) {
+        // Andreas version:
+        return audioRepository.findById(id).get();
 
-        for (Audio foundAudioItem : audioRepository.findAll())
+        // Min version:
+        /*for (Audio foundAudioItem : audioRepository.findAll())
         {
             if (foundAudioItem.getId() == id) {
                 return foundAudioItem;
             }
-        }
+            else {
+                errorResponse();
+            }
+        }*/
 
+        /*
         if (!audioRepository.existsById(id)) {
             new DocumentationResponse("Item Not Found.");
         }
+        */
 
-        //return audioRepository.findById(id).orElseThrow(() -> new DocumentationResponse("Item Not Found"));
+        //return null;
+    }
 
+    public Audio updateAudioItem(Audio audioItemToUpdate) {
+        return audioRepository.save(audioItemToUpdate);
+    }
+
+    /*
+    public void deleteAudioItem(Audio audioItemToDelete) {
+
+        if (audioRepository.findAll().contains(audioItemToDelete)) {
+            audioRepository.delete(audioItemToDelete);
+        }
+
+        //return audioRepository.deleteById(audioItemToDelete);
+    }
+    */
+
+    public void deleteById(Integer id) {
+        audioRepository.deleteById(id);
+    }
+
+    // TODO: 2018-05-17: Implement methods below. Not critical for the REST API app at this time.
+    public List<Audio> findByArtist(String artist){
         return null;
     }
 
@@ -102,10 +117,6 @@ public class AudioServiceImpl {
                 return audioItem;
             }
         }
-        return null;
-    }
-
-    public List<Audio> findByArtist(String artist){
         return null;
     }
 
