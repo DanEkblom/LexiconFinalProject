e13c7c380aa5bb4368a6f1433714bb60f3a135e3
Dan <dhgekblom@gmail.com>
2018-05-18 13:55:01 +0200

    JavaDoc comments added/updated

--- src/main/java/com/danekblom/lexicon/finalprojectrest/controllers/WebAudio.java
+++ src/main/java/com/danekblom/lexicon/finalprojectrest/controllers/WebAudio.java
@@ -18,12 +18,45 @@ public class WebAudio {
     @Autowired
     AudioServiceImpl audioService;
 
-    @RequestMapping("")
+    /**
+     * GET Method. Returns a static HTML page containing "how to use" instructions
+     * @return "audioInfo", HTML/Thymeleaf document
+     */
+    @GetMapping("")
     public String getAudioRoot() {
-        return "audioinfo";
+        return "audioInfo";
     }
 
     /**
+     * C in CRUD, Step 1
+     * GET Method for fetching the "add audio item" form page
+     * Mapped to /add
+     * @param model Spring class
+     * @return "addAudio", HTML/Thymeleaf document
+     */
+    @GetMapping("/add")
+    public String addAudioForm(Model model) {
+        model.addAttribute("audioItem", new Audio());
+        return "addAudio";
+    }
+
+    /**
+     * C in CRUD, Step 2
+     * POST Method for adding an audioService item via the addAudio.html Thymeleaf template.
+     * Mapped to /itemAdded, which is the form action url
+     * @param audioItem Audio class item to be added. Validated before added.
+     * @param model Spring class
+     * @return "addAudio", HTML/Thymeleaf document
+     */
+    @PostMapping("/itemAdded")
+    public String addAudio(@Valid Audio audioItem, Model model) {
+        model.addAttribute("audioItem", new Audio());
+        model.addAttribute("addedAudioItem", audioService.addAudioItem(audioItem));
+        return "addAudio";
+    }
+
+    /**
+     * R in CRUD.
      * HTTP GET Method for listing all audioService items in the allAudio.html Thymeleaf template
      * Mapped to /all
      * @param model Spring class
@@ -33,26 +66,6 @@ public class WebAudio {
     public String getAllAudio(Model model) {
         model.addAttribute("audioList", audioService.listAllAudioItems());
         return "allAudio";
-    }
-
-    @GetMapping("/add")
-    public String addAudio(Model model) {
-        model.addAttribute("audioItem", new Audio());
-        return "addAudio";
-    }
-
-    /**
-     * POST Method for creating/adding an audioService item via the addAudio.html Thymeleaf template.
-     * Mapped to /add
-     * @param audioItem Audio class item to be added. Validated before added.
-     * @param model Spring class
-     * @return "allAudio", HTML/Thymeleaf document
-     */
-    @PostMapping("/add")
-    public String getAudio(@Valid Audio audioItem, Model model) {
-        model.addAttribute("audioItem", new Audio());
-        model.addAttribute("addedAudioItem", audioService.addAudioItem(audioItem));
-        return "addAudio"; // addedAudio? New page showing the added item.
     }
 
 }
