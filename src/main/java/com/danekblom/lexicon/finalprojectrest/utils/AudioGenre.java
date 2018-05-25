37915514a75a21188941f4b70ec421025293e704
Dan <dhgekblom@gmail.com>
2018-05-25 15:45:48 +0200

    Constructor, values and getter added

--- src/main/java/com/danekblom/lexicon/finalprojectrest/utils/AudioGenre.java
+++ src/main/java/com/danekblom/lexicon/finalprojectrest/utils/AudioGenre.java
@@ -1,13 +1,22 @@
 package com.danekblom.lexicon.finalprojectrest.utils;
 
 public enum AudioGenre {
-    AUDIOBOOK,
-    CLASSICAL,
-    INDUSTRIAL,
-    INSTRUMENTAL,
-    METAL,
-    POP,
-    ROCK
+    AUDIOBOOK("Audio Book"),
+    CLASSICAL("Classical"),
+    INDUSTRIAL("Industrial Metal"),
+    INSTRUMENTAL("Instrumental"),
+    METAL("Metal"),
+    POP("Pop"),
+    ROCK("Rock");
 
-    //TODO: Rewrite this class like Type.java in Thymeleaf STSM example project
+    private final String genreName;
+
+    AudioGenre(String genreName) {
+        this.genreName = genreName;
+    }
+
+    public String getGenreName() {
+        return genreName;
+    }
+
 }
